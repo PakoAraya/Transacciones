fc4d1e175173526f28188ca826eb549c54af602b
franarayah <franarayah@gmail.com>
2025-01-10 21:16:49 -0300

    Se genera cambio en el controller para poder hacer los cambios de estados

--- transaccion/src/main/java/com/ejercicio/transaccion/controllers/TransaccionController.java
+++ transaccion/src/main/java/com/ejercicio/transaccion/controllers/TransaccionController.java
@@ -1,11 +1,14 @@
 package com.ejercicio.transaccion.controllers;
 
 import com.ejercicio.transaccion.interfaces.TransaccionService;
+import com.ejercicio.transaccion.model.Transaccion;
 import com.ejercicio.transaccion.services.TransaccionServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TransaccionController {
@@ -19,5 +22,15 @@ public class TransaccionController {
   public String index(Model model){
     model.addAttribute("transacciones", transaccionServiceImpl.getAllTransacciones());
     return "index";
+  }
+
+  //Controller para cambiar el estado
+  @PostMapping("/transaccion/{id}/cambiarEstado")
+  public String cambiarEstado(@PathVariable Long id, Model model){
+    Transaccion transaccion = transaccionServiceImpl.getAllTransaccionesById(id).orElse(null);
+    if(transaccion != null){
+      transaccionServiceImpl.updateEstado(id, !transaccion.isEstado());
+    }
+    return "redirect:/";
   }
 }
