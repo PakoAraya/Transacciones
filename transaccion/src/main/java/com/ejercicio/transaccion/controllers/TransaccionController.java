82c9f7ff1f22d39084cd85e7db4dc73325ea5d26
franarayah <franarayah@gmail.com>
2025-01-10 21:02:52 -0300

    Se crea el primer controller y la vista index.html asociado al mismo para cambiar estado de activo a inactivo

--- transaccion/src/main/java/com/ejercicio/transaccion/controllers/TransaccionController.java
+++ transaccion/src/main/java/com/ejercicio/transaccion/controllers/TransaccionController.java
@@ -1,5 +1,23 @@
 package com.ejercicio.transaccion.controllers;
 
+import com.ejercicio.transaccion.interfaces.TransaccionService;
+import com.ejercicio.transaccion.services.TransaccionServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TransaccionController {
 
+  @Autowired
+  private TransaccionService transaccionService;
+  @Autowired
+  private TransaccionServiceImpl transaccionServiceImpl;
+
+  @GetMapping("/")
+  public String index(Model model){
+    model.addAttribute("transacciones", transaccionServiceImpl.getAllTransacciones());
+    return "index";
+  }
 }
